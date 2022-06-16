5fdfd7a5b2a05fd0b9de3f71dd632375967db9df
Matthew Gennings <matthewgennings@gmail.com>
2022-06-16 00:30:28 -0500

    feat: added bootstrap5 + auth work + sidenav scss cleanup

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,37 +1,24 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
-import { Amplify, Hub } from 'aws-amplify';
-import { AuthFormField as AuthFormFields } from '@aws-amplify/ui-components';
+import { Auth, Hub } from 'aws-amplify';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
-import { pipe, Observable, take, map } from 'rxjs';
-import { tap, switchMap, filter, takeUntil } from 'rxjs/operators';
 
-Hub.listen('auth', (data) => {
-    switch (data.payload.event) {
-        case 'signIn':
-            console.log('user signed in');
-            // do something
-            break;
-        case 'signUp':
-            console.log('user signed up');
-            break;
-        case 'signOut':
-            console.log('user signed out');
-            break;
-        case 'signIn_failure':
-            console.log('user sign in failed');
-            break;
-        case 'configured':
-            console.log('the Auth module is configured');
-    }
-});
+export interface User {
+    username: string;
+    email: string;
+    verified: boolean;
+    userPool: string;
+}
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
     styleUrls: ['./auth.component.scss'],
 })
-export class AuthComponent implements OnInit {
-    @Output() signOutEvent = new EventEmitter<void>();
+export class AuthComponent implements OnInit, AfterViewInit {
+    @Output() authEvent = new EventEmitter<string>();
+    // The '!' is a workaround for error '<object> has no initializer and is not definitely assigned in the constructor'
+    currentUser!: User;
+
     public formFields = {
         signUp: {
             email: {
@@ -66,7 +53,48 @@ export class AuthComponent implements OnInit {
             }
         },
     };
+
     constructor(private router: Router, public authenticator: AuthenticatorService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        Hub.listen('auth', (data) => {
+            switch (data.payload.event) {
+                case 'signIn':
+                    console.info(`User ${this.currentUser.username} signed in at ${window.location.href}`);
+                    break;
+                case 'signUp':
+                    console.info(`User ${this.currentUser.username} signed up`);
+                    break;
+                case 'signOut':
+                    console.info(`User ${this.currentUser.username} signed out`);
+                    break;
+                case 'signIn_failure':
+                    console.error('Sign in failure. Error: ' + data.payload.message);
+                    break;
+                case 'configured':
+                    console.log('Congrats! Auth module is configured');
+            }
+        });
+    }
+
+    // Used once the page is fully loaded, great for data retrieval and initialization
+    ngAfterViewInit(): void {
+        Auth.currentAuthenticatedUser({
+            bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+        })
+            .then((user) => {
+                this.currentUser = {
+                    username: user.username,
+                    email: user.attributes.email,
+                    verified: user.attributes.email_verified,
+                    userPool: user.pool.userPoolId,
+                };
+            })
+            .catch((err) => console.log(err));
+    }
+
+    // Unused for now as Auth is the parent-most component of the app, saving for a future use case
+    userDetails(value: string) {
+        this.authEvent.emit(value);
+    }
 }
